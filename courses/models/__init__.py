d886e4db125806d9f9b975f069907065bd2d8330
AmirMustafa10 <amirmustafa0001@gmail.com>
2026-02-14 02:36:58 +0200

    Update  courses/models/__init__.py

--- courses/models/__init__.py
+++ courses/models/__init__.py
@@ -1,4 +1,4 @@
 # courses/models/__init__.py
 
-from .course_models import Subject, Course, Chapter, Education
-from .lesson_models import Lesson
+from .course_models import *
+from .lesson_models import *
