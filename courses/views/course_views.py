b7f92e9ac5894c5b69e8ff290ae51f1ca10231b2
AmirMustafa10 <amirmustafa0001@gmail.com>
2026-02-19 22:32:04 +0200

    add view sets for model course

--- courses/views/course_views.py
+++ courses/views/course_views.py
@@ -1,4 +1,87 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import Prefetch, Count, Q, Avg
+from django.core.cache import cache
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.shortcuts import get_object_or_404
+
+from ..models.course_models import Course, Education, Subject
+from ..Serializers.course_serializers import (
+    CourseCreateUpdateSerializer,
+    CourseDetailSerializer,
+    CourseListSerializer,
+    EducationDetailSerializer,
+    EducationListSerializer,
+    SubjectDetailSerializer,
+    SubjectListSerializer
+)
+from users.permissions import (
+    IsTeacherOrReadOnly,
+    IsTeacherOwnerOrReadOnly,
+    IsStudentOrTeacher,
+    IsStudent,
+    IsEnrollmentOwner,
+    IsLessonCourseTeacherOrReadOnly,
+)
 
 
+class CourseViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsTeacherOrReadOnly, IsTeacherOwnerOrReadOnly]
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return CourseListSerializer
+        if self.action == 'retrieve':
+            return CourseDetailSerializer
+        if self.action in ['create', 'update', 'partial_update']:
+            return CourseCreateUpdateSerializer
+        return CourseDetailSerializer  # Default serializer
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = Course.objects.select_related('education', 'teacher', 'subject')
+
+        if self.action == 'list':
+            if user.is_authenticated and user.user_type == 'teacher':
+                return queryset.filter(teacher__user=user)
+            return queryset.filter(is_published=True, is_active=True)
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(teacher=self.request.user.teacher_profile)
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.soft_delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    # @action(detail=True, methods=['post'], permission_classes=[IsStudent])
+    # def enroll(self, request, pk=None):
+    #     course = self.get_object()
+    #     user = request.user
+
+    #     # Check if the user is already enrolled
+    #     if Enrollment.objects.filter(course=course, student=user).exists():
+    #         return Response({'detail': 'You are already enrolled in this course.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    #     # Create the enrollment
+    #     Enrollment.objects.create(course=course, student=user)
+    #     return Response({'detail': 'Enrollment successful.'}, status=status.HTTP_201_CREATED)
+    
+    # @action(detail=True, methods=['post'], permission_classes=[IsStudent])
+    # def unenroll(self, request, pk=None):   
+    #     course = self.get_object()
+    #     user = request.user
+
+    #     # Check if the user is enrolled
+    #     enrollment = Enrollment.objects.filter(course=course, student=user).first()
+    #     if not enrollment:
+    #         return Response({'detail': 'You are not enrolled in this course.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    #     # Delete the enrollment
+    #     enrollment.delete()
+    #     return Response({'detail': 'Unenrollment successful.'}, status=status.HTTP_200_OK)
+    
+    
